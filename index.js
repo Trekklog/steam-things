50fe71e50c7a1b22e634dda42cf9c8285c49e5d6
Trekklog <trekklog@gmail.com>
2018-11-27 04:53:41 +0200

    added marketByName feature

--- index.js
+++ index.js
@@ -3,12 +3,52 @@ const game_search = require('./src/game-search')
 
 module.exports = {
     async search(game){
+
+        if (!game) {
+            return {
+                error: 'You must enter a game name.'
+            }
+        }
+
+        if (String(game).length > 40) {
+            return {
+                error: `Lenght limit is 40 characters.`
+            }
+        }
+
         const value = await game_search.fetch_store(game)
         return value
     },
 
     async market(gameAPPID){
+
+        if (!gameAPPID) {
+            return {
+                error: 'You must enter a Game APPID.'
+            }
+        }
+
+        if (!Number(gameAPPID)) {
+            return {
+                error: `'${gameAPPID}' is not a Game APPID.`
+            }
+        }
+
         const value = await game_items.getMarketItems(gameAPPID)
         return value
+    },
+
+    async marketByName(game){
+        const res = await this.search(game)
+        if (res.error) return res
+        else{
+            if (res.count === 0) return {
+                error: 'Cannot find a game with that name.'
+            }
+            else{
+                const items = this.market(res.games[0].game_id)
+                return items
+            }
+        }
     }
 }
--- tests/basic.js
+++ tests/basic.js
@@ -1,18 +1,45 @@
 const s_things = require('../index.js')
 
+const test = {
+    game: 'Life is strange',
+    appid: 319630 // 319630 = Life is Strange
+}
 
-// 319630 = Life is Strange
-s_things.market(319630).then(items => {
-    console.log(items.count)
-    // 37
-    console.log(items.items)
+
+// search market item for a given APPID
+s_things.market(test.game).then(items => {
+     // check for an error, if there is no error print the result
+    if (items.error) console.log(items.error)
+    else{
+        console.log(items.items)
+        console.log(items.count)
+    }
 
 }).catch(err => {
     console.log(`[ERR - Market] ${err.name} -- ${err.message}`)
 })
 
-// s_things.search('Life is Strange').then(games => {
-//     console.log(games)
-// }).catch(err => {
-//     console.log(`[ERR - Search] ${err.name} -- ${err.message}`)
-// })
+
+// search games with the given input
+s_things.search(test.appid).then(games => {
+    //  check for an error, if there is no error print the result
+    if (games.error) console.log(games.error)
+    else{
+        console.log(games)
+    }
+}).catch(err => {
+    console.log(`[ERR - Search] ${err.name} -- ${err.message}`)
+})
+
+
+// search market items for specific game, first element that matches with the input will be used
+s_things.marketByName(test.game).then(res => {
+    if (res.error) console.log(res.error)
+    else{
+        console.log(res.items)
+        console.log(res.count)
+    }
+}).catch(err => {
+         console.log(`[ERR - marketByName] ${err.name} -- ${err.message}`)
+})
+    
