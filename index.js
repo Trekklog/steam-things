2b8ba13a919f0fd628152536e7128251cd71ed9c
Trekklog <trekklog@gmail.com>
2018-11-27 06:11:20 +0200

    marketByName updated

--- index.js
+++ index.js
@@ -38,7 +38,28 @@ module.exports = {
         return value
     },
 
-    async marketByName(game){
+    async marketByName(game, games_limit, delay){
+
+        // verify games_limit
+        if (Number(games_limit)){
+            games_limit = Math.abs(games_limit)
+        }
+        else{
+            games_limit = 1
+        }
+
+        if (Number(delay)){
+            delay = Math.abs(delay)
+
+            if (delay < 500) {
+                console.log("Delay changed to 500")
+                delay = 500
+            }
+        }
+        else{
+            delay = 1500
+        }
+
         const res = await this.search(game)
         if (res.error) return res
         else{
@@ -46,9 +67,39 @@ module.exports = {
                 error: 'Cannot find a game with that name.'
             }
             else{
-                const items = this.market(res.games[0].game_id)
-                return items
+
+                let items = []
+
+                for (let index = 0; index < games_limit; index++) {
+                    await this.wait(delay) // delay between each request
+
+                    if (res.count < index) {
+                        return {
+                            items,
+                            games_count: items.length,
+                        }
+                    }
+
+                    const market_search = await this.market(res.games[index].game_id)
+                    if (market_search.count !== 0) {
+                        items.push({
+                            game_name: res.games[index].game_name,
+                            game_appid: res.games[index].game_id,
+                            items: market_search 
+                        })
+                    }
+                }
+                return {
+                    items,
+                    games_count: items.length,
+                }
             }
         }
+    },
+
+    wait(time){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve(), time)
+        })
     }
 }
